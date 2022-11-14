17793dcfaa23dcb42a23f67893ae2e2e899dc9a9
ByteTrail <brandon.grooters@bytetrail.com>
2022-11-13 18:21:58 -0600

    VAUX-53 #time 1h #comment session management updates

--- vaux-broker/src/broker/session.rs
+++ vaux-broker/src/broker/session.rs
@@ -1,14 +1,17 @@
 use std::time::{Duration, Instant};
 
+use super::DEFAULT_KEEP_ALIVE;
+
 const DEFAULT_SESSION_EXPIRY_INTERVAL: u64 = 60 * 30; // 30 minute session expiry
 const DEFAULT_SESSION_SCAN_PERIOD: u64 = 60; // scan for expired sessions 1 minute
 
 #[derive(Debug, Clone)]
 pub struct Session {
-    pub id: String,
+    id: String,
     last_active: Instant,
-    max_keep_alive: Duration,
-    pub expiry: Duration,
+    connected: bool,
+    keep_alive: Duration,
+    pub session_expiry: Duration,
 }
 
 impl Session {
@@ -17,8 +20,9 @@ impl Session {
         Session {
             id,
             last_active: Instant::now(),
-            max_keep_alive,
-            expiry: Duration::new(0, 0),
+            connected: true,
+            keep_alive: Duration::from_secs(DEFAULT_KEEP_ALIVE),
+            session_expiry: Duration::new(0, 0),
         }
     }
 
@@ -26,9 +30,22 @@ impl Session {
         Session {
             id,
             last_active: Instant::now(),
-            max_keep_alive: Duration::from_secs(max_keep_alive_secs as u64),
-            expiry: Duration::from_secs(expiry_secs as u64),
+            connected: true,
+            keep_alive: Duration::from_secs(max_keep_alive_secs as u64),
+            session_expiry: Duration::from_secs(expiry_secs as u64),
         }
+    }
+
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+
+    pub fn connected(&self) -> bool {
+        self.connected
+    }
+
+    pub fn set_connected(&mut self, connected: bool) {
+        self.connected = connected;
     }
 
     /// Sets the last session activity to the time that the method is invoked.
@@ -37,18 +54,12 @@ impl Session {
     }
 
     /// Gets the maximum keep alive in seconds.
-    pub fn max_keep_alive_secs(&self) -> u64 {
-        self.max_keep_alive.as_secs()
+    pub fn keep_alive(&self) -> u64 {
+        self.keep_alive.as_secs()
     }
     /// Sets the maximum session keep alive to the time passed in seconds.
-    pub fn set_max_keep_alive_secs(&mut self, secs: u64) {
-        self.max_keep_alive = Duration::from_secs(secs);
-    }
-
-    /// Returns true if the session keep alive time exceeds the session maximum
-    /// keep alive time based on the std::time::Instant that the method is invoked.
-    pub fn max_keep_alive_exceeded(&self) -> bool {
-        self.last_active.elapsed() > self.max_keep_alive
+    pub fn set_keep_alive(&mut self, secs: u64) {
+        self.keep_alive = Duration::from_secs(secs);
     }
 }
 
